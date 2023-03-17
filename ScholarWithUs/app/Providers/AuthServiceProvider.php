765819ee533a3ad2a546b3d643c04e5e89df4895
reyhanmichiels <michiels.reyhan@gmail.com>
2023-03-17 10:53:34 +0700

    DEPLOY Authorization

--- ScholarWithUs/app/Providers/AuthServiceProvider.php
+++ ScholarWithUs/app/Providers/AuthServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\Program;
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +25,16 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('only-Admin', function(User $user) {
+            return $user->role == 'admin';
+        });
+
+        Gate::define('user-program', function(User $user, Program $program){
+            if ($user->programs->find($program->id == null)) {
+                return false;
+            }
+
+            return true;
+        });
     }
 }
